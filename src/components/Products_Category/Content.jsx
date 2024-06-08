0bcfa324e3c7b94ce81b7ef809562dd6966476f3
kaesuu22 <153926494+kaesuu22@users.noreply.github.com>
2024-06-08 10:59:55 +0800

    Update Content.jsx

--- src/components/Products_Category/Content.jsx
+++ src/components/Products_Category/Content.jsx
@@ -6,12 +6,14 @@ import axios from 'axios';
 import apiEndpoints from '../api_endpoints';
 import ProductList from './ProductList';
 
+// Extracting categoryParam from URL params
 function Content() {
   let { categoryParam } = useParams();
+  // State variables for categories and product counts
   const [categories, setCategories] = useState([]);
 const [countNew, setCountNew] = useState([]);
 const [countSale, setCountSale] = useState([]);
-
+ // Fetching categories data from API endpoint
 useEffect(() => {
   axios.get(apiEndpoints.find(endpoint => endpoint.name === 'get_categories').url)
     .then(response => {
@@ -21,7 +23,7 @@ useEffect(() => {
       console.error('Error fetching categories:', error);
     });
 }, []);
-
+// Fetching counts for new and sale products
 useEffect(() => {
   axios.get(apiEndpoints.find(endpoint => endpoint.name === 'get_countnewprods').url)
     .then(response => {
@@ -39,18 +41,19 @@ useEffect(() => {
       console.error('Error fetching sale product count:', error);
     });
 }, []);
-
+  // Calculating total product count, sale count, and new count
 const totalProductCount = categories.reduce((acc, curr) => acc + Number(curr.count), 0);
 const totalSaleCount = countSale.reduce((acc, curr) => acc + Number(curr.count), 0);
 const totalNewCount = countNew.reduce((acc, curr) => acc + Number(curr.count), 0);
 
-
+ // Rendering the component
   return (
     <>
       <div id="sns_content" className="wrap layout">
         <div className="container">
           <div className="row">
             <div id="sns_left" className="col-md-3">
+                {/* Left sidebar */}
               <div className="wrap-in"></div>
               <div className="block block-layered-nav block-layered-nav--no-filters">
                 <div className="block-title">
@@ -58,11 +61,13 @@ const totalNewCount = countNew.reduce((acc, curr) => acc + Number(curr.count), 0
                     <span>Shop By</span>
                   </strong>
                 </div>
+                 {/* Category filters */}
                 <div className="block-content toggle-content">
                   <dl id="narrow-by-list">
                     <dt className="odd">Category</dt>
                     <dd className="odd">
                       <ul>
+                         {/* Filter for new products */}
                         <li style={{ lineHeight: "15px", width:'100%' }}>
                           <NavLink to='/allProducts/New' style={{ textDecoration: 'none', color: 'inherit' }}>
                             <label style={{ display: 'flex'}}>
@@ -74,6 +79,7 @@ const totalNewCount = countNew.reduce((acc, curr) => acc + Number(curr.count), 0
                             </label>
                           </NavLink>
                         </li>
+                         {/* Filter for sale products */}
                         <li style={{ lineHeight: "15px", width:'100%' }}>
                           <NavLink to='/allProducts/Sale' style={{ textDecoration: 'none', color: 'inherit' }}>
                             <label style={{ display: 'flex'}}>
@@ -85,6 +91,7 @@ const totalNewCount = countNew.reduce((acc, curr) => acc + Number(curr.count), 0
                             </label>
                           </NavLink>
                         </li>
+                        {/* Filter for all products */}
                         <li style={{ lineHeight: "15px", width:'100%' }}>
                           <NavLink to='/allProducts' style={{ textDecoration: 'none', color: 'inherit' }}>
                             <label style={{ display: 'flex'}}>
@@ -96,6 +103,7 @@ const totalNewCount = countNew.reduce((acc, curr) => acc + Number(curr.count), 0
                             </label>
                           </NavLink>
                         </li>
+                         {/* Dynamic rendering of category filters */}
                         {categories.map((category, index) => (
                           <li key={index} style={{ lineHeight: "15px"  }}>
                             <NavLink to={`/allProducts/${category.prod_category.replace(/[^/]+$/, category.prod_category)}`} style={{ textDecoration: 'none', color: 'inherit' }}>
@@ -120,6 +128,7 @@ const totalNewCount = countNew.reduce((acc, curr) => acc + Number(curr.count), 0
                 </a>
               </div>
             </div>
+             {/* Product list */}
             <ProductList />
           </div>
         </div>
